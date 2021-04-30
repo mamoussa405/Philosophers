a5dae50fb54f11528077eaf88117039a29f90952
Mohamed Amoussaoui <mamoussa@e2r8p2.1337.ma>
2021-04-30 16:48:55 +0000

    header realisation

---
+++ philo_one/philo_one.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamoussa <mamoussa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/30 16:09:21 by mamoussa          #+#    #+#             */
+/*   Updated: 2021/04/30 16:47:29 by mamoussa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+typedef struct  s_data
+{
+    size_t  number_of_philo;
+    size_t  time_to_die;
+    size_t  time_to_eat;
+    size_t  time_to_sleep;
+    size_t  num_of_t_to_eat;
+}               t_data;
